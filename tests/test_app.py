77cdd71ca71ee0c3da9bea58c93741356aaec138
jidnya26 <jidnya2006@gmail.com>
2026-09-22 10:39:58 +0530

    Fix database initialization in tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,25 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from app import app
+import app as app_module
+
+app = app_module.app
+
+TEST_DATABASE = "tests/test_notices.db"
+
+app_module.DATABASE = TEST_DATABASE
+
+
+def setup_module():
+    if os.path.exists(TEST_DATABASE):
+        os.remove(TEST_DATABASE)
+
+    app_module.init_db()
+
+
+def teardown_module():
+    if os.path.exists(TEST_DATABASE):
+        os.remove(TEST_DATABASE)
 
 
 def test_home_page():
